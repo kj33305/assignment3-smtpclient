55a148615befd733a012d076fffa377b8532e5b6
kj33305 <98822946+kj33305@users.noreply.github.com>
2022-02-10 09:56:09 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,6 +11,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Fill in start
     clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect(mail_server)
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
